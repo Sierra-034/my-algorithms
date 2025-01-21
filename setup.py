1eb60cd8d81b165a8f99b0f71a7405868633c404
Samuel Gómez <samuel.gomez.balderas@gmail.com>
2025-01-20 19:50:24 -0600

    fix setup.py to correct installation

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='algorithms',
     version='0.1',
-    packages=find_packages(),
+    packages=find_packages(where='src'),
     package_dir={'': 'src'},
     install_requires=[
         # Add your dependencies here
